0a72284a119adbe9cb6758711949cacb7d53c3c9
jsar26606@gmail.com <jsar26606@whatever.com>
2023-04-10 01:59:16 -0700

    ammendments to the class Paddle

--- Engine/Paddle.cpp
+++ Engine/Paddle.cpp
@@ -1,6 +1,44 @@
 #include "Paddle.h"
+#include "Graphics.h"
 
-void Paddle::moveUp()
+void Paddle::clampToScreen()
 {
-	
+	locy += delta;
+
+	const int bottom = locy + getHeight();
+	if (locy < 0)
+	{
+		locy = 0;
+	}
+	else if (bottom >= Graphics::ScreenHeight)
+	{
+		locy = (Graphics::ScreenHeight - 1) - getHeight();
+	}
+}
+
+void Paddle::update(Keyboard& kbd)
+{
+	if (kbd.KeyIsPressed(VK_DOWN))
+	{
+		locy += delta;
+	}
+	if (kbd.KeyIsPressed(VK_UP))
+	{
+		locy -= delta;
+	}
+}
+
+void Paddle::drawPaddle(Graphics& gfx)
+{
+	gfx.DrawRect(locx, locy, width, height, Colors::Green);
+}
+
+int Paddle::getHeight()
+{
+	return height;
+}
+
+int Paddle::getWidth()
+{
+	return width;
 }
--- Engine/Paddle.h
+++ Engine/Paddle.h
@@ -1,15 +1,19 @@
 #pragma once
+#include "Keyboard.h"
+#include "Graphics.h"
 
 class Paddle
 {
 public:
-	int locx;
-	int locy;
-	void moveUp();
-	void moveDown();
+	int locx = 30;
+	int locy = 300;
+	void clampToScreen();
+	void update(Keyboard& kbd);
+	void drawPaddle(Graphics& gfx);
+	int getHeight();
+	int getWidth();
 private:
 	static constexpr int height = 40;
 	static constexpr int width = 40;
 	static constexpr int delta = 1;
-
 };
